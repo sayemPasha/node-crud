a03aa23d4918911b3c7755b7b0bb3ddd5a05b76f
Sayem Pasha <frostbiteengineering@gmail.com>
2019-10-04 19:58:08 +0600

    create,read single and all entry,update and delete

---
+++ index.js
@@ -0,0 +1,84 @@
+/*
+1. carefull about the link: don't miss starting / (forward slash)
+2. 200:ok, 400:bad request upon create/update body, 404:resource not found
+*/
+const express = require('express');
+const app = express();
+const Joi = require('@hapi/joi');
+
+//enable json in express
+app.use(express.json());
+
+let courses = [
+    {id:1, name:"cat"},
+    {id:2, name: "dog"}
+];
+
+//init port
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`listening to port ${port}`);
+});
+
+//read all entry
+app.get('/api/courses', (request, response) => {
+    response.send(courses);
+});
+
+
+//read single entry
+app.get('/api/courses/:id', (request, response)=> {
+    
+    const course = courses.find(course=> course.id === parseInt(request.params.id));
+    if(!course) return response.status(400).send("invalid id");
+
+    response.send(course);
+
+});
+
+//create an entry
+app.post('/api/courses/', (request,response) => {
+    const {error} = validateCourse(request.body); //destructuring
+    if(error) return resposne.status(400).send(error.details[0]);
+
+    const newEntry = {
+        id : courses.length + 1, //FATAL ISSUE: if any entry is deleted, then length shortended, so again entry may overlap ID
+        name: request.body.name
+    };
+    courses.push(newEntry);
+
+    response.send(newEntry);
+    
+});
+
+//update an entry
+app.put('/api/courses/:id', (request, response) => {
+
+    const course = courses.find(course=> course.id === parseInt(request.params.id));
+    const {error} = validateCourse(request.body);
+    if(error) return response.status(400).send(error.details[0]);
+    course.name = request.body.name;
+    
+    response.send(course);
+});
+
+//delete
+app.delete('/api/courses/:id', (request, response) =>{
+    const course = courses.find(course => course.id === parseInt(request.params.id));
+    if(!course) return response.status(400).send("invalid id");
+
+    //extract absolute index
+    const courseIndex = courses.indexOf(course);
+    courses.splice(courseIndex, 1);
+
+    response.send(course);
+    
+});
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required() //this is a string and it has 3 characters and it is required
+    });
+
+    return schema.validate(course);
+}
